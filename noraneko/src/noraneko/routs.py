d3bc5ca8a37b6ebe5de48424f63f42eb60f1dbd4
rychanya <rychanya@gmail.com>
2024-05-07 14:10:52 +0000

    💩

--- noraneko/src/noraneko/routs.py
+++ noraneko/src/noraneko/routs.py
@@ -1,3 +1,4 @@
+import asyncio
 from collections.abc import AsyncIterable
 
 from blacksheep import FromQuery, Request, Router
@@ -9,13 +10,29 @@ from noraneko.queue_wrapper import QueueManager, QueueWrapper
 router = Router()
 
 
+# @router.get("/events")
+# async def home(request: Request, queue_manager: QueueManager, uid: FromQuery[str]) -> AsyncIterable[ServerSentEvent]:
+#     async def stop() -> bool:
+#         if is_stopping() or await request.is_disconnected():
+#             return True
+#         return False
+
+#     with QueueWrapper(uid.value, queue_manager, stop) as queue:
+#         async for q in queue:
+#             yield q
+
+
 @router.get("/events")
 async def home(request: Request, queue_manager: QueueManager, uid: FromQuery[str]) -> AsyncIterable[ServerSentEvent]:
-    async def stop() -> bool:
-        if is_stopping() or await request.is_disconnected():
-            return True
-        return False
+    stop_event = asyncio.Event()
 
-    with QueueWrapper(uid.value, queue_manager, stop) as queue:
-        async for q in queue:
-            yield q
+    async def stop_task():
+        while True:
+            if is_stopping() or await request.is_disconnected():
+                stop_event.set()
+
+    asyncio.create_task(stop_task())
+
+    while True:
+        await asyncio.sleep(1)
+        yield ServerSentEvent(data="ghhg")
